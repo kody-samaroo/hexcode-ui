a62944a322992910b18101486164c1869af87f89
Kody Samaroo <kody.samaroo@gmail.com>
2023-09-09 18:42:28 -0400

    multiple choice answers layout

--- src/Pages/Answers.js
+++ src/Pages/Answers.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import '../Styles/Answers.css';
 
-export default function Answers({ hexcode }) {
-
-    const answers = [];
-
-    const hexcodeDigits = [
-        "0","1","2","3","4","5","6","7","8","9",
-        "A","B","C","D","E","F"
-    ]
-    
-    const generateHexcodes = () => {
-        for(let i=0;i<3;i++){
-            let red = hexcodeDigits[Math.floor(Math.random() * 15)] + hexcodeDigits[Math.floor(Math.random() * 15)];
-            let green = hexcodeDigits[Math.floor(Math.random() * 15)] + hexcodeDigits[Math.floor(Math.random() * 15)];
-            let blue = hexcodeDigits[Math.floor(Math.random() * 15)] + hexcodeDigits[Math.floor(Math.random() * 15)];
-            answers.push("#"+red+green+blue);
-        }
-        answers.push(hexcode);
-    }
+export default function Answers({ choices }) {
 
 
 
   return (
-    <div className="parent">
-        <div className="child">Test</div>
-        <div className="child">Test</div>
-        <div className="child">Test</div>
-        <div className="child">Test</div>
+    <div className="answers-container">
+        {choices.map(choice => (
+            <div className="answer">{choice}</div>
+        ))}
     </div>
   )
 }
