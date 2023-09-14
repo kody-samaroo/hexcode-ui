c31b17984e5413c69a2bf8ce82f2ab927d1672d9
Kody Samaroo <kody.samaroo@gmail.com>
2023-09-14 00:48:07 -0400

    bootstrap style sheet

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,4 +1,5 @@
 import '../Styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -56,23 +57,23 @@ function App() {
           answer={answer}
           answerHandler={answerHandler}
         />
-        <Modal
-          show={showModal}
+        <Modal 
+          show={showModal} 
           onHide={handleClose}
           backdrop="static"
           keyboard={false}
         >
-          <Modal.Body>
-            I will not close if you click outside me. Don not even try to press
-            escape key.
-          </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button variant="primary">
-                Understood
-              </Button>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
           </Modal.Footer>
         </Modal>
       </div>
