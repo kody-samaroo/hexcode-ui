f932198aecc746c0499c9264a77863ad44fbfcc3
Kody Samaroo <kody.samaroo@gmail.com>
2023-09-23 18:19:49 -0400

    finalized version

--- src/Pages/Display.js
+++ src/Pages/Display.js
@@ -5,7 +5,8 @@ export default function Display({ answer }) {
 
   return (
     <div className="display-container">
-        <div className="display-window" style={{backgroundColor: `${answer}`}}/>
+      <div className="display-text">Guess the color</div>
+      <div className="display-window" style={{backgroundColor: `${answer}`}}/>
     </div>
   )
 }
